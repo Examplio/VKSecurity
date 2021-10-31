c11289b07ae48eb74674f5f389ada09e13439295
Victor Kasko <88190385+Examplio@users.noreply.github.com>
2021-10-31 19:01:56 +0200

    Update _config.php

--- VK BOT/_config.php
+++ VK BOT/_config.php
@@ -2,9 +2,9 @@
 
 require_once('simplevk-master/autoload.php');
 
-const VK_KEY = "6767e16916fb13c0bd32e7f0c2ac3722970813f99406afbbc083d8ce95407a37111a250afb875a16e4ba7"; // EN: Group token with messages permission | RU: Токен группы с правами на сообщения
+const VK_KEY = ""; // EN: Group token with messages permission | RU: Токен группы с правами на сообщения
 const VERSION = "5.131"; // EN: API version. Leave 5.131 for correct work | RU: Версия API. Оставь 5.131 для корректной работы
-const CONFIRM_STR = "f75ffd9a"; // EN: Confirmation String. | RU: Строка подтверждения
+const CONFIRM_STR = ""; // EN: Confirmation String. | RU: Строка подтверждения
 
 use DigitalStar\vk_api\vk_api;
 use DigitalStar\vk_api\Execute;
